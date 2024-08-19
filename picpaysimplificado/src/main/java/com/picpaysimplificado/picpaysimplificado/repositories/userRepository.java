160720e4465a6ecdbc990df4985da2e21828cd7b
LuanLucasGon <lucasluangon@gmail.com>
2024-08-19 18:13:27 -0300

    feat: Creating the user service and fixing incorrect environment variables

--- picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/userRepository.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/userRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface userRpository extends JpaRepository<User, Long> {
+public interface userRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
     Optional<User> findUserById(Long id);
 }
